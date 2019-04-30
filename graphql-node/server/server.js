0f66d388a601a8dd7ef8fa4ce5f30dbfca3b3c16
Carlo Gilmar <carlogilmar12@gmail.com>
2019-04-29 21:47:48 -0500

    ERROR fix error and implement npm task for run the  server

--- graphql-node/server/server.js
+++ graphql-node/server/server.js
@@ -2,7 +2,7 @@ const express = require('express'); // Require express module
 const PORT = 3500; // Server port
 const app = express(); // Create an express application
 app.use('/graphql', (req, res)=> { // Add a route handler
-    res.send( Welcome to Our Authors App");
+    res.send("Welcome to Our Authors App");
 });
 app.listen(PORT, ()=> {
     console.log("Server Running on Port:", PORT);
