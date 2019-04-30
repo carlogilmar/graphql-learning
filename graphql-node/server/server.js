630db215f0fba097150d0049607751afb27a8bb1
Carlo Gilmar <carlogilmar12@gmail.com>
2019-04-29 22:11:22 -0500

    At this moment this server runs graphiql well

--- graphql-node/server/server.js
+++ graphql-node/server/server.js
@@ -1,10 +1,12 @@
 const express = require('express'); // Require express module
 const PORT = 3500; // Server port
 const app = express(); // Create an express application
-app.use('/graphql', (req, res)=> { // Add a route handler
-    res.send("Welcome to Our Authors App");
-});
+const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+const bodyParser = require('body-parser');
+
+
 app.listen(PORT, ()=> {
     console.log("Server Running on Port:", PORT);
 });
-
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
