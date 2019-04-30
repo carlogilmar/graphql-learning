dd637a49a783980e06e53e107cf5bbe22eb4c181
Carlo Gilmar <carlogilmar12@gmail.com>
2019-04-29 21:44:06 -0500

    Adding example using express

---
+++ graphql-node/server/server.js
@@ -0,0 +1,10 @@
+const express = require('express'); // Require express module
+const PORT = 3500; // Server port
+const app = express(); // Create an express application
+app.use('/graphql', (req, res)=> { // Add a route handler
+    res.send( Welcome to Our Authors App");
+});
+app.listen(PORT, ()=> {
+    console.log("Server Running on Port:", PORT);
+});
+
